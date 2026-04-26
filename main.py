ba0686d39d13c93ea77ac223b1e370d41af442a4
Eunice-projects <beltraneunice05@gmail.com>
2026-04-26 13:05:06 -0600

    Importando reportes.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from lectura_datos import *
+from reportes import *
 
 ruta="encuesta_restaurante.csv" #Aqui almacenamos la ruta del archivo
 
 datos=cargar_datos(ruta) #Mandamos a llamar la funsion que lee el archivo y donde la esta estructura
-
 
 
